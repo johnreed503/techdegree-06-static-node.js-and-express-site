1c8b067796c7af4d10fdf9239ed583842d8d8621
John Reed <contact.johnreed@gmail.com>
2021-03-08 22:40:47 -0500

    cleaned up comments

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # techdegree-06-static-node.js-and-express-site
- 
+
+A portfolio website that uses static JSON to serve up links to github, linkedin and images
+
+CSS changes:
+-removed border for profile pick
+-adjusted borders on projects on main page to be wider and without a line around them
+-removed borders on individual projects
--- app.js
+++ app.js
@@ -1,42 +1,37 @@
+
+//sets up express and sets up pug templating
 const express = require('express');
 const app = express();
 app.set('view engine', 'pug');
-
 app.use(express.static('public'));
 
+//pulls in json data
 const projectdata = require('./data.json')
 const projects = projectdata.projects
 
-
-
-// app.use((req, res, next) => {
-//   console.log('hello from errors')
-//   const err = new Error('oh noes!');
-//   err.status = 500;
-//   next(err);
-// });
-
-
-
+//route for home page, passes in projects variable with all project data
 app.get('/', (req, res) => {
   res.render('index', {projects});
 });
 
+//route for about page
 app.get('/about', (req, res) => {
   res.render('about');
 });
 
+//route for project page, passes in individual project based on the :id
 app.get('/project/:id', (req, res) => {
   res.render('project', {project: projects[req.params.id]});
 });
 
-
+//handles errors when a page is searched for but not found
 app.use((req, res, next) => {
   const err = new Error('Unfortunatly the internet could not find the page you were looking for')
   err.status = 404
   next(err);
 })
 
+//handles errors
 app.use((err, req, res, next) => {
   if (err.status === 404) {
     res.status(err.status)
@@ -46,7 +41,6 @@ app.use((err, req, res, next) => {
   res.render('error', {error: err});
 }
 })
-
 
 
 app.listen(3000, () => {
